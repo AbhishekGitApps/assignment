33bc1671710a495b0b9de7e8ae43ce7e17b6389d
Abhishek Singh <abhishek912255@gmail.com>
2022-10-20 08:20:45 +0530

    Initial comit

---
+++ Admin/conn.php
@@ -0,0 +1,9 @@
+<?php
+	$link=mysqli_connect("localhost","id19736861_abhishek","?Y=eQGUYob6-Nmqj","id19736861_project");
+	if(mysqli_connect_error())
+	{
+		echo "Connection error".mysqli_connect_error();
+		exit;
+	}
+	
+?>
